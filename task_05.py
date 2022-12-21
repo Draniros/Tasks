4747e89af1197e9fb9efd1c81e0bee73f9fedbde
Draniros <120799431+Draniros@users.noreply.github.com>
2022-12-21 18:07:19 +0300

    Update task_05.py

--- task_05.py
+++ task_05.py
@@ -1,16 +1,14 @@
 from datetime import datetime
 from datetime import timedelta
 
-class Task:
+def __date_in_future__(uday):
 
-    def __date_in_future__(uday):
-        
-        today = datetime.now()
-        try:
-            future = today
-            future = future + timedelta(days=int(uday))
-        finally:
-            return future.strftime("%d-%m-%Y %H:%M:%S");
+    today = datetime.now()
+    try:
+        future = today
+        future = future + timedelta(days=int(uday))
+    finally:
+        return future.strftime("%d-%m-%Y %H:%M:%S");
 
 print("This programm see future")
 print("If you write non-integers programm do erorr")
