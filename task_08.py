025d8ad4b819aa8367ab244606c3573ddf495038
Draniros <120799431+Draniros@users.noreply.github.com>
2022-12-21 18:14:26 +0300

    Update task_08.py

--- task_08.py
+++ task_08.py
@@ -26,9 +26,9 @@ def multiply_numbers(inp = None):
         return None
 
 
-print(t.multiply_numbers()) # => None
-print(t.multiply_numbers('ss')) # => None
-print(t.multiply_numbers('1234')) # => 24
-print(t.multiply_numbers('sssdd34')) # => 12
-print(t.multiply_numbers(2.3)) # => 6
-print(t.multiply_numbers([5, 6, 4])) # => 120)
+print(multiply_numbers()) # => None
+print(multiply_numbers('ss')) # => None
+print(multiply_numbers('1234')) # => 24
+print(multiply_numbers('sssdd34')) # => 12
+print(multiply_numbers(2.3)) # => 6
+print(multiply_numbers([5, 6, 4])) # => 120)
