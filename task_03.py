3943723e3710f3de5f8f17cf12fc3d625af38f50
Draniros <120799431+Draniros@users.noreply.github.com>
2022-12-21 18:06:45 +0300

    Update task_03.py

--- task_03.py
+++ task_03.py
@@ -1,21 +1,19 @@
-class Task:
+def max_odd(array):
 
-    def max_odd(array):
-        
-        i = len(array)-1 
-        maxs = 0
+    i = len(array)-1 
+    maxs = 0
 
-        while i != -1:
-            if(isinstance((array[i]),int|float)):
-                per = int(array[i])
-                if(per%2 == 1):
-                    if(per > maxs): maxs = per
-            i-=1
+    while i != -1:
+        if(isinstance((array[i]),int|float)):
+            per = int(array[i])
+            if(per%2 == 1):
+                if(per > maxs): maxs = per
+        i-=1
 
-        if(maxs == 0):
-            return "None"
-        else:
-            return maxs
+    if(maxs == 0):
+        return "None"
+    else:
+        return maxs
 
 t = Task
 print(t.max_odd([7, 'ololol', None, 9.00, 3, 22]))
