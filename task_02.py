3ee2331abedd0d12a282143cf577eb24faec704b
Draniros <120799431+Draniros@users.noreply.github.com>
2022-12-21 18:03:17 +0300

    Update task_02.py

--- task_02.py
+++ task_02.py
@@ -1,21 +1,19 @@
-class Task:
-
-    def coincidence(ulist = [],urange = []):
-        llist = []
-        lrange = []
-        if(ulist == [] or urange == []):
-            return llist
-        else:
-            for n in urange:
-                lrange.append(n)
-            start = min(lrange)
-            stop = max(lrange)+1
-            for i in ulist:
-                if(isinstance(i, (int, float))):
-                    if(i >= start and i < stop):
-                        llist.append(i)
-            llist.sort()
-            return llist
+def coincidence(ulist = [],urange = []):
+    llist = []
+    lrange = []
+    if(ulist == [] or urange == []):
+        return llist
+    else:
+        for n in urange:
+            lrange.append(n)
+        start = min(lrange)
+        stop = max(lrange)+1
+        for i in ulist:
+            if(isinstance(i, (int, float))):
+                if(i >= start and i < stop):
+                    llist.append(i)
+        llist.sort()
+        return llist
 T = Task
 lst = [5.65,7,7.2,9,4,2,20,8]
 print(T.coincidence([1, 2, 3, 4, 5], range(3, 6)))
